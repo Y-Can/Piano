135fb2f7f986babbd6482b31da44c9abb6d601d3
Can <71851732+Y-Can@users.noreply.github.com>
2021-08-22 06:45:11 +0200

    Add files via upload

---
+++ PianoJS/main.js
@@ -0,0 +1,117 @@
+var noteMusique = document.getElementById("white b")
+const audioB = document.getElementById("audio b")
+
+
+noteMusique.addEventListener("click", function(){
+audioB.play()
+})
+
+var noteMusique = document.getElementById("black as")
+const audioA = document.getElementById("audio a")
+
+
+noteMusique.addEventListener("click", function(){
+audioA.play()
+})
+
+var noteMusique = document.getElementById("white a")
+const audioC = document.getElementById("audio c")
+
+
+noteMusique.addEventListener("click", function(){
+audioC.play()
+})
+
+var noteMusique = document.getElementById("black gs")
+const audioD = document.getElementById("audio d")
+
+
+noteMusique.addEventListener("click", function(){
+audioD.play()
+})
+
+
+var noteMusique = document.getElementById("white g")
+const audioE = document.getElementById("audio e")
+
+
+noteMusique.addEventListener("click", function(){
+audioE.play()
+})
+
+var noteMusique = document.getElementById("black fs")
+const audioF = document.getElementById("audio f")
+
+
+noteMusique.addEventListener("click", function(){
+audioF.play()
+})
+
+var noteMusique = document.getElementById("white f")
+const audioG = document.getElementById("audio g")
+
+
+noteMusique.addEventListener("click", function(){
+audioG.play()
+})
+
+var noteMusique = document.getElementById("white e")
+const audioH = document.getElementById("audio h")
+
+
+noteMusique.addEventListener("click", function(){
+audioH.play()
+})
+
+var noteMusique = document.getElementById("black ds")
+const audioI = document.getElementById("audio i")
+
+
+noteMusique.addEventListener("click", function(){
+audioI.play()
+})
+
+var noteMusique = document.getElementById("white d")
+const audioJ = document.getElementById("audio j")
+
+
+noteMusique.addEventListener("click", function(){
+audioJ.play()
+})
+
+var noteMusique = document.getElementById("black cs")
+const audioK = document.getElementById("audio k")
+
+
+noteMusique.addEventListener("click", function(){
+audioK.play()
+})
+
+var noteMusique = document.getElementById("white c")
+const audioL = document.getElementById("audio l")
+
+
+noteMusique.addEventListener("click", function(){
+audioL.play()
+})
+
+
+
+
+
+var noteMusique = document.getElementById("parasite")
+const audioParasite = document.getElementById("parasite son")
+
+
+noteMusique.addEventListener("click", function() {
+audioParasite.play()
+})
+
+var Pause = document.querySelector("button")
+Pause.addEventListener("click", function(){
+    audioParasite.pause()
+})
+    
+
+
+
